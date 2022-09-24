5ab5667c09d82688cde039552cc6abf8c6937c04
ssups <shalstlqruki@gmail.com>
2022-09-24 23:21:47 +0900

    0924

--- 20220914/test/src/index.js
+++ 20220914/test/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // 최초의 전체를 감싸는 dom 하나는 들고와줘야함
 
+// root 안에 App 컴포넌트를 렌더링한다
 root.render(
     // 최초로 만든 html dom인 root dom 안에 render로 다른 컴포넌트들을 계속해서 넣어준다
     // React.StrictMode 오류나 로그를 띄워주는 아이 (엄격모드?) , 콘솔도 2번 찍어줌
